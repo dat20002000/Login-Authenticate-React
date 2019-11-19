d3f1c3f0711c65671b0f416af111da16cc6572ed
Dat Nguyen <dat20002000@gmail.com>
2019-11-19 15:47:44 -0500

    fake-backend

--- src/_helpers/fake-backend.js
+++ src/_helpers/fake-backend.js
@@ -1,4 +1,4 @@
-let user = JSON.parse(localStorage.getItem('users') || []);
+let users = JSON.parse(localStorage.getItem('users') || []);
 
 export function configureFakeBackEnd() {
     let realFetch = window.fetch;
@@ -28,7 +28,59 @@ export function configureFakeBackEnd() {
 
                     return;
                 }
-            })
+
+                if (url.endsWith('/users') && opts.method === 'GET') {
+                    if (opts.headers && opts.headers.Authorization === 'Bearer fake-jwt-token') {
+                        resolve({ok: true, text: () => JSON.stringify(user)});
+                    } else {
+                        rejects('Unauthorised');
+                    }
+
+                    return;
+                }
+
+                if (url.endsWith('/users/register') && opts.method === 'POST') {
+                    let newUser = JSON.parse(opts.body);
+
+                    let duplicateUser = users.filter(user => { return user.username === newUser.username; }).length;
+                    if (duplicateUser) {
+                        rejects('Username "' + newUser.username + '" is already taken!');
+                        return;   
+                    }
+
+                    newUser.id = users.length ? Math.max(...users.map(user => user.id)) + 1 : 1;
+                    users.push(newUser);
+                    localStorage.setItem('users', JSON.stringify(users));
+
+                    resolve( {ok: true, text: () => Promise.resolve() });
+
+                    return;
+                }
+
+                if (url.match(/\/users\/\d+$/) && opts.method === 'DELETE') {
+                    if (opts.headers && opts.headers.Authorization === 'Bearer fake-jwt-token') {
+                        let urlParts = url.split('/');
+                        let id = this.parseInt(urlParts[urlParts.length - 1]);
+
+                        for (let i = 0; i < users.length; i++) {
+                            let user = users[i];
+                            if (user.id === id) {
+                                users.splice(i ,1);
+                                localStorage.setItem('users', JSON.stringify(users));
+                                break;
+                            }                            
+                        }
+
+                        resolve( {ok: true, text: () => Promise.resolve()});
+                    } else {
+                        rejects('Unauthorised');
+                    }
+
+                    return;
+                }
+
+                realFetch(url, opts).then(response => resolve(response));
+            }, 500);
         })
     }
 }
